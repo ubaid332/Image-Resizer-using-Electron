e9fd41f1acfb11c47a5cfe753af75a2d885953ad
Ubaid Ullah <ubaid332@yahoo.com>
2024-04-03 14:11:06 +0500

    rendered the main page

---
+++ main.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const {app, BrowserWindow} = require('electron');
+
+function createMainWindow(){
+    const mainWindow = new BrowserWindow({
+        title: "Image Resizer",
+        width: 600,
+        height: 600,
+
+    });
+
+    mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+}
+
+app.whenReady().then(() => {
+    createMainWindow();
+});
