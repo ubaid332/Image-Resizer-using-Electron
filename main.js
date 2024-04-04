87d26e9bf7a6ac44008f4c973ea2302e6645374d
Ubaid Ullah <ubaid332@yahoo.com>
2024-04-04 12:16:01 +0500

    Used ipc for renderer and main process

--- main.js
+++ main.js
@@ -1,17 +1,93 @@
 const path = require('path');
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, Menu, ipcMain} = require('electron');
 
+const isMac = process.platform === 'darwin';
+const isDev = process.env.NODE_ENV !== "development";
+
+// create the main window
 function createMainWindow(){
     const mainWindow = new BrowserWindow({
         title: "Image Resizer",
-        width: 600,
-        height: 600,
+        width: isDev? 1000: 600,
+        height: 400,
+        webPreferences: {
+            contextIsolation: true,
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'preload.js')
+          }
 
     });
+
+    // open devtools if in dev env
+    if(isDev){
+        mainWindow.webContents.openDevTools();
+    }
 
     mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
 
+function createAboutWindow(){
+    const aboutWindow = new BrowserWindow({
+        title: "Image Resizer",
+        width: 300,
+        height: 300,
+        
+
+    });
+
+
+    aboutWindow.loadFile(path.join(__dirname, "./renderer/about.html"));
+}
+
+// Menu template
+const menu = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Quit',
+                click: () => app.quit(),
+                accelerator: 'CmdorCtrl+w',
+            }
+        ]
+    },
+
+    {
+        label: 'Help',
+        submenu:[
+            {
+                label: 'About',
+                click: createAboutWindow
+            }
+        ]
+    }
+]
+
+// when app is ready
 app.whenReady().then(() => {
     createMainWindow();
+
+    // implement menu
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow()
+        }
+      })
 });
+
+
+// Respond to ipcRenderer resize
+
+ipcMain.on('image:resize', (e, options)=>{
+    console.log(options)
+})
+
+
+app.on('window-all-closed', () => {
+    if (!isMac) {
+      app.quit()
+    }
+  })
---
+++ renderer/js/renderer.js
@@ -0,0 +1,94 @@
+
+const form = document.querySelector('#img-form');
+const img = document.querySelector('#img');
+const outputPath = document.querySelector('#output-path');
+const filename = document.querySelector('#filename');
+const heightInput = document.querySelector('#height');
+const widthInput = document.querySelector('#width');
+
+function loadImage(e){
+    const file = e.target.files[0];
+
+    if(!isFileImage(file)){
+        alertError("Please select an image");
+        return;
+    }
+
+    // Get original dimensions
+    const image = new Image();
+    image.src = URL.createObjectURL(file);
+    image.onload = function(){
+        heightInput.value = this.height;
+        widthInput.value = this.width;
+        outputPath.innerText = path.join(os.homedir(), 'imageresizer')
+    }
+
+    form.style.display = "block";
+    filename.innerText = file.name;
+}
+
+function sendImage(e){
+    e.preventDefault();
+
+    const width = widthInput.value;
+    const height = heightInput.value;
+    const imgPath = img.files[0].path;
+
+
+    if(!img.files[0]){
+        alertError("Please upload an image");
+        return;
+    }
+
+    if(width === '' || height === ''){
+        alertError('Please fill in the height and width');
+        return;
+    }
+
+    // Send to main using ipcRenderer
+
+    ipcRenderer.send('image:resize', {
+        imgPath,
+        width,
+        height
+    })
+
+
+}
+
+// Make sure file is image
+function isFileImage(file){
+    const acceptedImageTypes = ['image/gif', 'image/png', 'image/jpeg'];
+    return file && acceptedImageTypes.includes(file['type']);
+}
+
+function alertError(message){
+    Toastify.toast({
+        text: message,
+        duration: 5000,
+        close: false,
+        style: {
+            background: 'red',
+            color: 'white',
+            textAlign: 'center'
+        }
+    })
+}
+
+
+function alertSuccess(message){
+    Toastify.toast({
+        text: message,
+        duration: 5000,
+        close: false,
+        style: {
+            background: 'green',
+            color: 'white',
+            textAlign: 'center'
+        }
+    })
+}
+
+img.addEventListener('change', loadImage);
+
+form.addEventListener('submit', sendImage);
